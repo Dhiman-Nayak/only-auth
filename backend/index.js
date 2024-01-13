bd17b4276e16f783faf6f7a6c2bf0359cf8fa9a5
Dhiman <dhimannayak003@gmail.com>
2024-01-13 18:19:25 +0530

    gg

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express  from "express";
-
+import dotenv from "dotenv"
 const app = express()
+dotenv.config()
 
-app.listen(5151,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Running");
 })
